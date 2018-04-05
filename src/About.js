86b8fc2d5e5f8871ddb322191ed5a0e5e9b9599c
Anna Birrer <aubirrer@gmail.com>
2018-04-05 16:36:21 +0200

    Updated about us page

--- src/About.js
+++ src/About.js
@@ -1,5 +1,74 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default function About() {
-    return <h1>You are in the About Us.</h1>;
+    return (
+        <div id="about">
+            <h1>About Huntington Beach Landscape</h1>
+            <div id="about__info-box">
+                <p>
+                    <h3>Long Story Short</h3>
+                    Huntington Beach Landscape Company has been locally owned
+                    and operated since 1981. The owner, Urs Birrer, come from a
+                    lineage of landscapers with his father and brothers all
+                    operating and owning their own landscape firms across
+                    Switzerland and California. Urs was trained in Switzerland
+                    and later settled in Southern California with his family.
+                    With over 30 years of experience, Urs specializes in
+                    designing custom landscapes with quality plants and
+                    materials for all kinds of residential and commercial
+                    projects.
+                    <br />
+                    <br />
+                    When he's not building people's dream gardens, Urs is off
+                    sailing along the Californian coast, learning to fly planes,
+                    or repairing all kinds of machines, engines, and computers.
+                </p>
+                <img src="./images/about/about2.jpg" />
+            </div>
+
+            <div id="contact__outer-box">
+                <div>
+                    <iframe
+                        width="400"
+                        height="300"
+                        frameBorder="0"
+                        style={{ border: "0" }}
+                        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyALk1IaXTkZAIB4FGqJ7khg9JCTJdZK8gg
+                    &q=Orange+County,+CA,+USA"
+                    />
+                </div>
+                <div id="contact__info-box">
+                    <div>
+                        <h3>Service Area</h3>
+                        <p>
+                            Huntington Beach Landscape mainly services the
+                            Orange County and Los Angeles County regions of
+                            Southern California.
+                        </p>
+                    </div>
+                    <div>
+                        <div>
+                            <h3>Contact Info</h3>
+                            <p>
+                                Phone:{" "}
+                                <a href="tel:+17148466000">(714) 846-6000</a>
+                                <br />
+                                Email:{" "}
+                                <a href="mailto:hblandscapeco@gmail.com">
+                                    hblandscapeco@gmail.com{" "}
+                                </a>
+                                <br />
+                                Location: Huntington Beach, CA 92649
+                            </p>
+                        </div>
+
+                        <button>
+                            <Link to="/contact">Get In Touch</Link>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
